b5775f3818d4553daf62708e1a17923bd52bffe5
Shubhy9399 <shubhangipokley@gmail.com>
2020-06-10 20:46:20 +0530

    again modified with leap year

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ function nameCheck(){
 
 }
 }
+function LeapYearCheck(){
+
+   let c=document.getElementById("year").value;
+   if((c%400==0)||(c%4==0 && c%100!=0))
+		{
+                                       alert( c+ " is a Leap Year!");
+		}
+    else {
+                alert(c + " is not a Leap Year!")
+            }
+}
